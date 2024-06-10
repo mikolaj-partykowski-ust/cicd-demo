cc5e0bda88efbad4fbfe81834fcf21409b5a1e9a
Mikolaj Partykowski <mikolaj.partykowski@ust.com>
2024-06-10 10:56:44 +0000

    Added husky pre-commit & linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,27 @@
-const cds = require('@sap/eslint-plugin-cds')
+const cds = require("@sap/eslint-plugin-cds");
+const js = require("@eslint/js");
+const globals = require("globals");
 
 module.exports = [
   cds.configs.recommended,
   {
     plugins: {
-      '@sap/cds': cds
+      "@sap/cds": cds,
     },
     rules: {
-      ...cds.configs.recommended.rules
-    }
-  }
-]
+      ...cds.configs.recommended.rules,
+    },
+    files: ["srv/*.cds", "db/schema.cds"],
+  },
+  {
+    files: ["srv/*.js"],
+    rules: {
+      ...js.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+];
